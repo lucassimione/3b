eff52a2e3b5a1b80a17171bc78843754363874a4
04025276232 <04025276232@JIPA-LABIN03-29.ifro.local>
2018-05-04 16:58:47 -0400

    Instalando o Hibernate no Projeto

--- 3b_LucasSimione/src/br/edu/ifro/MenuPrincipalController.java
+++ 3b_LucasSimione/src/br/edu/ifro/MenuPrincipalController.java
@@ -13,6 +13,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 /**
  *
@@ -25,6 +29,8 @@ public class MenuPrincipalController implements Initializable {
     
     @FXML
     private Button btnSoma;
+    @FXML
+    private Label label;
     
     @FXML
     private void soma(ActionEvent event) {
@@ -39,5 +45,25 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+    try{
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),900,682);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastrar Aluno");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+    }
     
 }
